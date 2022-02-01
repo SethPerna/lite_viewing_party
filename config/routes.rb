24c984d28679f575b817d2b66fee3e734b1287e9
Seth Perna <gseth26@gmail.com>
2022-02-01 17:16:20 -0500

    add function to button for user discover

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get '/register', to: 'users#new'
   resources :users, only: %i[show create]
+  get '/users/:id/discover', to: 'users#discover'
 end
