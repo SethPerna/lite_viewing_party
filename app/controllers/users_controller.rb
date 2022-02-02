e35c0a39b1f8aa56ee19a51186972d8a414a21c9
Darren Kulback <dkulback@gmail.com>
2022-02-01 17:47:44 -0700

    top rated movies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,22 @@ class UsersController < ApplicationController
   end
 
   def discover
-    
+    @user = User.find(params[:id])
+    if params[:top_rated].present?
+      conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+        binding.pry
+        faraday.headers['apikey'] = ENV['movie_api_key']
+      end
+      response = conn.get('/3/movie/top_rated')
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      binding.pry
+      movies = data[:results][0][:members]
+
+      found_members = members.find_all { |m| m[:last_name] == params[:search] }
+      @member = found_members.first
+      render 'discover'
+    end
   end
 
   private
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -68,4 +68,10 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('fake_api_key') { ENV['movie_api_key'] }
+  config.configure_rspec_metadata!
 end
