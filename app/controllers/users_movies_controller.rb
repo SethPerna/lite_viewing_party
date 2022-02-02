35554d6e7de2c248358594e23ae2a07faf2afedf
Darren Kulback <dkulback@gmail.com>
2022-02-02 14:30:03 -0700

    reroute to user_movies

---
+++ app/controllers/users_movies_controller.rb
@@ -0,0 +1,16 @@
+class UsersMoviesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    if params[:top_rated].present?
+      @top_movies = TopRated.new.movies
+      render 'users/movies/index'
+    end
+  end
+
+  def create
+    if params[:search].present?
+      @search_movie = SearchMovie.new.search(params[:search])
+      render 'users/movies/index'
+    end
+  end
+end
--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Discover Page' do
-  it 'has a button to discover top rated movies' do
+  it 'has a button to discover top rated movie' do
     user = User.create!(name: 'user', email: 'email')
 
     VCR.use_cassette('top_rated_movies') do
@@ -11,17 +11,30 @@ RSpec.describe 'Discover Page' do
 
         expect(current_path).to eq(user_movies_path(user))
       end
+      within '.top-rated-movies' do
+        expect(page.status_code).to eq(200)
+        expect(page).to have_content('Your Eyes Tell')
+        expect(page).to have_content('The Lord of the Rings: The Return of the King')
+      end
     end
   end
-  it 'has a button to discover top rated movies' do
+
+  it 'has a form to search for movies' do
     user = User.create!(name: 'user', email: 'email')
+
     VCR.use_cassette('search_movies') do
       visit "/users/#{user.id}/discover"
       within '.discover-movies' do
         fill_in 'search', with: 'Shaw'
+
         click_on 'Search'
 
         expect(current_path).to eq(user_movies_path(user))
+      end
+
+      within '.search-movie' do
+        expect(page.status_code).to eq(200)
+        expect(page).to have_content('Fast & Furious Presents: Hobbs & Shaw')
       end
     end
   end
