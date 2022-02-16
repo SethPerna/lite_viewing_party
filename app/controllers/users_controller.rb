cc3a12fc67bf102cdcdcf6ada9ecaa503bd651d9
Seth Perna <gseth26@gmail.com>
2022-02-16 17:33:07 -0500

    fix login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,14 +26,14 @@ class UsersController < ApplicationController
   end
 
   def login_user
-    if user = User.find_by(email: params[:email])
-      user.authenticate(params[:password])
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = "Welcome #{user.email}"
-      redirect_to "/users/#{user.id}"
+      flash[:success] = "Welcome #{user.name}"
+      redirect_to user_path(user)
     else
       flash[:error] = "Invalid email/password"
-      redirect_to "/login"
+      render :login_form
     end
   end
 
